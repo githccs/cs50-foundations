7e51dd84feb3fba6ff2e89940d7699d3695a69f3
Mr. Choo <cchongsian@gmail.com>
2026-01-27 20:57:57 +0800

    Add points.c: Conditional comparisons
    Implementing a program to compare user-inputted points against a fixed value using if, else if, and else statements. Focuses on control flow and basic integer input.

---
+++ points.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    const int MINE = 2;
+    int points = get_int("How many points did you lose?: \n");
+
+    if (points < mine)
+    {
+        printf("You lost fewer points than me.\n");
+    }
+
+    else if (points > mine)
+    {
+        printf("You lost more points than me.\n");
+    }
+
+    else
+    {
+        printf("You lost same points as me.\n)");
+    }
+
+}
